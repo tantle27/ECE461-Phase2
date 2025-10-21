fc8695e5b2e3fbd809b4312f3707e469b35cd823
Trevor <antletrevor5@gmail.com>
2025-10-21 14:23:35 -0400

    ci lambda

--- app/lambda_handler.py
+++ app/lambda_handler.py
@@ -5,6 +5,7 @@ import awsgi
 # Cold-start initialization (keeps app warm)
 flask_app = create_app()
 
+
 def handler(event, context):
     """AWS Lambda entrypoint translating API Gateway requests to Flask WSGI."""
     return awsgi.response(flask_app, event, context)
