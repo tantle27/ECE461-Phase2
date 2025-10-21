e3528eb3a33a27a92f3cb040298ff8127259a573
Trevor <antletrevor5@gmail.com>
2025-10-21 14:52:25 -0400

    wrong library

--- app/lambda_handler.py
+++ app/lambda_handler.py
@@ -1,6 +1,7 @@
-# lambda_handler.py
 import logging
-import awsgi
+from typing import Any, Dict
+
+import aws_wsgi  # type: ignore[import-untyped]
 from app.app import create_app
 
 flask_app = create_app()
@@ -8,6 +9,6 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 
-def handler(event, context):
-    log.info("Lambda URL event: %s", event.get("rawPath"))
-    return awsgi.response(flask_app, event, context)
+def handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
+    log.info("Lambda event: %s", event.get("rawPath") or event.get("path"))
+    return aws_wsgi.response(flask_app, event, context)
